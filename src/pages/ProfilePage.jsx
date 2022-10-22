130890c6c550b5093b4a883d2af6ff11f62dfbf7
IgorGorshkov <91373076+IgorGorshkov@users.noreply.github.com>
2022-10-22 11:00:27 +0300

    lesson-4

---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,16 @@
+function ProfilePage() {
+  return (
+    <h1
+      style={{
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      Profile Page
+    </h1>
+  );
+}
+
+export default ProfilePage;
